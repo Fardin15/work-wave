5e8b090134575a39c3dc8225a6f993d11fb95a19
Fardin15 <mohammedshowrav15@gmail.com>
2024-05-10 23:41:48 +0600

    private route created

---
+++ src/Privateroute/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return (
+      <div className="max-w-6xl mx-auto text-center">
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    );
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={location.pathname}></Navigate>;
+};
+
+export default PrivateRoute;
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};
